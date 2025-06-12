a568f46b20e833a214da61a9826813b2d214f3e3
phaer <hello@phaer.org>
2025-06-12 10:37:19 +0200

    run-vm: wrap to use repart-built image for the vm as well

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
   outputs = { self, nixpkgs }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    lib = nixpkgs.lib;
 
     nixosModules = {
       host = ./configuration.nix;
@@ -15,10 +16,28 @@
       epehmeral = ./ephemeral.nix;
       image = ./image.nix;
     };
-    modules = pkgs.lib.attrValues nixosModules;
+    modules = lib.attrValues nixosModules;
 
     vm = pkgs.nixos { imports = modules; };
-    run-vm = vm.config.system.build.vm;
+
+    # We wrap the upstream VM runner to pass it a pre-built
+    # disk image. This ensures we are testing the disk generation
+    # mechanism as well, instead of just mounting the hosts nix
+    # store to the VM.
+    run-vm = pkgs.writeShellScriptBin "run-vm" ''
+      export NIX_DISK_IMAGE="$(pwd)/nixos.img"
+
+      echo "generating $NIX_DISK_IMAGE"
+      ${pkgs.qemu}/bin/qemu-img \
+        create \
+        -f qcow2 \
+        -F raw \
+        -b "${vm.config.system.build.image}/${vm.config.image.fileName}" \
+        $NIX_DISK_IMAGE
+
+      echo "starting vm"
+      ${lib.getExe vm.config.system.build.vm}
+    '';
 
   in {
     inherit nixosModules;
