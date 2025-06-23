66fb56472a3a32bbc20da73c6a260fe64972df63
phaer <hello@phaer.org>
2025-06-23 23:18:42 +0200

    android-build-env: add binutils
    for debugging sandbox issues

--- android-build-env.nix
+++ android-build-env.nix
@@ -37,6 +37,7 @@ in
       buildAndroid
       # sudo apt-get install git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
       gitMinimal
+      binutils
       gnupg flex bison zip curl zlib xorg.libX11 mesa libxml2 libxslt unzip fontconfig openssl
       # Before you can work with AOSP, you must have installations of OpenJDK, Make, Python 3, and Repo
       jdk
