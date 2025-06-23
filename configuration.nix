07d898ecb0ee51788b44ac00525ca8aa24bc3451
phaer <hello@phaer.org>
2025-06-23 15:37:52 +0200

    Assume the disk is in /dev/vda, by default.
    Bare metal users have to adapt there paths. /dev/disk/by-diskseq does
    not seem reliable enough and we need to provide an explicit device
    path as we don't have / backed by a block device

--- configuration.nix
+++ configuration.nix
@@ -9,9 +9,7 @@
     system.name = "android-builder";
 
     boot.initrd.systemd.repart = {
-      device = "/dev/nvme0n1";
-      empty = "force";
-      discard = false;
+      device = "/dev/vda";
     };
 
     environment.systemPackages = with pkgs; [
