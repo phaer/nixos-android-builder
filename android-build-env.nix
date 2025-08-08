05c58856772444892f69aa2224f210a3714b5d72
phaer <hello@phaer.org>
2025-08-08 13:20:59 +0200

    android-build-env to nixos module

--- android-build-env.nix
+++ android-build-env.nix
@@ -1,6 +1,74 @@
-{ pkgs }:
+{ lib, pkgs, config, ...}:
 let
-  fetchAndroid = pkgs.writeShellScriptBin "fetch-android" ''
+  packages = with pkgs; [
+    # sudo apt-get install git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
+    gitMinimal
+    binutils
+    diffutils
+    findutils
+    gnupg
+    flex
+    bison
+    zip
+    curl
+    zlib
+    xorg.libX11
+    mesa
+    libxml2
+    libxslt
+    unzip
+    fontconfig
+    openssl
+    # Before you can work with AOSP, you must have installations of OpenJDK, Make, Python 3, and Repo
+    jdk
+    gnumake
+    python3
+    (git-repo.override { git = gitMinimal; })
+    rsync
+  ];
+
+  # pkgs.writeShellScriptBin with bashInteractive instead of pkgsruntimeShell, so that we
+  # don't get errors about the missing "complete" builtin.
+  writeShellScriptBin =
+    name: text:
+    pkgs.writeTextFile {
+      inherit name;
+      executable = true;
+      destination = "/bin/${name}";
+      text = ''
+        #!${pkgs.bashInteractive}/bin/bash
+        ${text}
+      '';
+      checkPhase = ''
+        ${pkgs.stdenv.shellDryRun} "$target"
+      '';
+      meta.mainProgram = name;
+    };
+
+    closure = pkgs.closureInfo {
+      rootPaths = packages;
+    };
+    rsyncExe = pkgs.lib.getExe pkgs.rsync;
+
+  libraries = pkgs.runCommandNoCC "android-libraries" {} ''
+      mkdir -p $out/lib/
+      for store_path in $(cat "${closure}/store-paths"); do
+        test -e "$store_path/lib" && echo "$store_path/lib" || true
+      done \
+      | xargs -i ${rsyncExe} -r --copy-dirlinks --links --chmod "+w" "{}"/ $out/lib/
+  '';
+
+  binaries = pkgs.runCommandNoCC "android-binaries" {} ''
+      set -x
+      mkdir -p $out/bin
+      cp ${pkgs.bashInteractive}/bin/bash $out/bin/
+      for store_path in $(cat "${closure}/store-paths"); do
+        test -e "$store_path/bin" && echo "$store_path/bin" || true
+      done \
+      | xargs -i ${rsyncExe} -r --copy-links --copy-unsafe-links "{}"/ $out/bin
+    '';
+
+  fetchAndroid = writeShellScriptBin "fetch-android" ''
     set -e
     mkdir -p $SOURCE_DIR
     cd $SOURCE_DIR
@@ -13,56 +81,67 @@ let
          -b android-latest-release \
          -u https://android.googlesource.com/platform/manifest
 
-    repo sync -c -j4 || true
-    repo sync -c -j4
-
-    patch -p1 < ${./fix_nsjail.patch}
+    repo sync -c $@ || true
+    repo sync -c $@
   '';
-  buildAndroid = pkgs.writeShellScriptBin "build-android" ''
+
+  buildAndroid = writeShellScriptBin "build-android" ''
     set -e
     cd $SOURCE_DIR
     source build/envsetup.sh || true
-    lunch aosp_cf_x86_64_only_phone-aosp_current-userdebug
+    lunch aosp_cf_x86_64_only_phone-aosp_current-eng
     m
   '';
-
 in
-pkgs.buildFHSEnv {
-  name = "android-build-env";
-  runScript = "bash";
-  profile = ''
-    export ANDROID_JAVA_HOME=${pkgs.jdk.home}
-    export SOURCE_DIR=$HOME/source
-    # We don't seem to have /lib in the linker cache by default here.
-    export LD_LIBRARY_PATH=/lib
-    # Set a custom prompt to more easily see in which shell we are.
-    export PROMPT_COMMAND='PS1="\e[0;32mandroid-build-env\$ \e[0m"'
-  '';
-  targetPkgs =
-    pkgs: with pkgs; [
-      fetchAndroid
-      buildAndroid
-      # sudo apt-get install git-core gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
-      gitMinimal
-      binutils
-      gnupg
-      flex
-      bison
-      zip
-      curl
-      zlib
-      xorg.libX11
-      mesa
-      libxml2
-      libxslt
-      unzip
-      fontconfig
-      openssl
-      # Before you can work with AOSP, you must have installations of OpenJDK, Make, Python 3, and Repo
-      jdk
-      gnumake
-      python3
-      (git-repo.override { git = gitMinimal; })
-      rsync
-    ];
-}
+  lib.mkMerge [
+    {
+      environment.variables = {
+        "ANDROID_JAVA_HOME" = pkgs.jdk.home;
+        "SOURCE_DIR" = "$HOME/source";
+      };
+      environment.systemPackages =
+        packages ++ [
+          fetchAndroid buildAndroid
+        ];
+    }
+    {
+      environment.variables = {
+        "PATH" = "/bin:$PATH";
+        # "ENVFS_RESOLVE_ALWAYS" = "1";
+      };
+
+      fileSystems."/bin" = {
+        device = "${toString binaries}/bin";
+        options = [ "bind" ];
+        fsType = "none";
+      };
+    }
+    {
+      environment.variables = {
+        "NIX_LD_LIBRARY_PATH" = lib.mkForce "/lib";
+        "NIX_LD_LOG" = "warn";
+      };
+      programs.nix-ld.enable = true;
+
+      fileSystems."/lib" = {
+        device = "${toString libraries}/lib";
+        options = [ "bind" ];
+        fsType = "none";
+      };
+    }
+    {
+      environment.systemPackages = [
+        pkgs.helix pkgs.zellij pkgs.ripgrep pkgs.fd pkgs.strace
+      ];
+
+      virtualisation.forwardPorts = [
+        { from = "host"; host.port = 2222; guest.port = 22; }
+      ];
+      services.openssh.enable = true;
+      users.users.root.openssh.authorizedKeys.keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLopgIL2JS/XtosC8K+qQ1ZwkOe1gFi8w2i1cd13UehWwkxeguU6r26VpcGn8gfh6lVbxf22Z9T2Le8loYAhxANaPghvAOqYQH/PJPRztdimhkj2h7SNjP1/cuwlQYuxr/zEy43j0kK0flieKWirzQwH4kNXWrscHgerHOMVuQtTJ4Ryq4GIIxSg17VVTA89tcywGCL+3Nk4URe5x92fb8T2ZEk8T9p1eSUL+E72m7W7vjExpx1PLHgfSUYIkSGBr8bSWf3O1PW6EuOgwBGidOME4Y7xNgWxSB/vgyHx3/3q5ThH0b8Gb3qsWdN22ZILRAeui2VhtdUZeuf2JYYh8L"
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCwT3A8zAcCGHNBVx0lhmz3Hhygs8XXszXqARj9QzHHs/fR3J55MTT/jk/GlmOnUzVj8RIPzzwru5J+8XgtBKU8="
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHqRNv8hueRuN4khLUQMiPVS0NqwZfX17BNXIRZJ9yRPAAAAE3NzaDpoZWxsb0BwaGFlci5vcmc="
+      ];
+    }
+]
