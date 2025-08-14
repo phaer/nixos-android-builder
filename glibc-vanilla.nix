c883fe3e1fb1c2ba907a8b311ee9e0a35f9a89c9
phaer <hello@phaer.org>
2025-08-14 08:47:20 +0200

    actually drop libgcc

--- glibc-vanilla.nix
+++ glibc-vanilla.nix
@@ -1,40 +1,4 @@
-{ lib, stdenv, glibc, linuxHeaders, libgcc }:
-
-let
-  customLibGcc = stdenv.mkDerivation {
-    pname = "libgcc-from-gcc-static";
-    inherit (libgcc) version buildInputs nativeBuildInputs;
-
-    src = libgcc.src;
-
-    outputs = [ "out" ];  # Only one output
-
-    dontFixup = true;     # No patchelf, no stripping, no rpath fiddling
-
-    configurePhase = ''
-      ./configure \
-        --disable-multilib \
-        --enable-languages=c \
-        --disable-bootstrap \
-        --with-glibc-version=${glibc.version} \
-        --disable-nls \
-        --enable-static \
-        --disable-shared
-    '';
-
-    CFLAGS   = libgcc.CFLAGS_FOR_BUILD or "";
-    CXXFLAGS = libgcc.CXXFLAGS_FOR_BUILD or "";
-
-    buildPhase = ''
-      make
-    '';
-
-    #installPhase = ''
-    #  mkdir -p $out/lib
-    #  cp libgcc/libgcc.a $out/lib/
-    #'';
-  };
-in
+{glibc, linuxHeaders}:
 glibc.overrideAttrs (oldAttrs: {
   separateDebugInfo = false;
   outputs = [ "out" ];
@@ -48,11 +12,6 @@ glibc.overrideAttrs (oldAttrs: {
     "--enable-kernel=6.12"
     "--disable-werror"
   ];
-
-  nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ customLibGcc ];
-
-  # We link with static libgcc.a, so add its path to LDFLAGS
-  NIX_LDFLAGS = "-L${customLibGcc}/lib -lgcc";
 
   dontFixup = true;
 
