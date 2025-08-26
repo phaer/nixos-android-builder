18e5630f190c4e74df8872ccf28cdcc233b90c28
phaer <hello@phaer.org>
2025-08-26 14:21:56 +0200

    encrypt: refactor, use scripts for ExecStart(Post)

--- encrypt-var-lib.nix
+++ encrypt-var-lib.nix
@@ -1,14 +1,24 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  waitForDisk = pkgs.writeScript "wait-for-disk" ''
+    #!/bin/sh
+    set -e
+    udevadm settle -t 5
+  '';
+  generateDiskKey = pkgs.writeScript "generate-disk-key" ''
+    #!/bin/sh
+    set -e
+    umask 0077
+    head -c 64 /dev/urandom > /etc/disk.key
+  '';
+in
 {
   config = {
-    # Location of the random key to encrypt the persistent volume with,
-    # should never touch the disk, /etc is on tmpfs
-    boot.initrd.systemd.repart.keyFile = "/etc/disk.key";
-
-    # --factory-reset instructs systemd-repart to reset all partitions marked with FactoryReset=true,
-    # only /var/lib in our case. The read-only partitions stay in place.
-    boot.initrd.systemd.repart.factoryReset = true;
-
     systemd.repart.partitions."30-var-lib".Encrypt = "key-file";
 
     boot.initrd.luks.devices."var_lib_crypt" = {
@@ -18,25 +28,41 @@
 
     fileSystems."/var/lib".device = lib.mkForce "/dev/mapper/var_lib_crypt";
 
-    boot.initrd.systemd.services.systemd-repart = {
-      before = [
-        "systemd-cryptsetup@var_lib_crypt.service"
-      ];
-      serviceConfig.ExecStartPost = "/bin/udevadm settle -t 5";
-    };
+    boot.initrd.systemd = {
+      # Location of the random key to encrypt the persistent volume with,
+      # should never touch the disk, /etc is on tmpfs
+      repart.keyFile = "/etc/disk.key";
 
-    boot.initrd.systemd.services.generate-disk-key = {
-      description = "Generate a secure, ephemeral key to encrypt the persistent disk with";
-      wantedBy = [ "initrd.target" ];
-      before = [ "systemd-repart.service" ];
-      requiredBy = [ "systemd-repart.service" ];
-      unitConfig = {
-        DefaultDependencies = false;
-      };
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStart = ''/bin/sh -c "umask 0077; head -c 64 /dev/urandom > /etc/disk.key"'';
+      # --factory-reset instructs systemd-repart to reset all partitions marked with FactoryReset=true,
+      # only /var/lib in our case. The read-only partitions stay in place.
+      repart.factoryReset = true;
+
+      storePaths = [
+        waitForDisk
+        generateDiskKey
+      ];
+      services = {
+        systemd-repart = {
+          before = [
+            "systemd-cryptsetup@var_lib_crypt.service"
+          ];
+          serviceConfig.ExecStartPost = waitForDisk;
+        };
+
+        generate-disk-key = {
+          description = "Generate a secure, ephemeral key to encrypt the persistent disk with";
+          wantedBy = [ "initrd.target" ];
+          before = [ "systemd-repart.service" ];
+          requiredBy = [ "systemd-repart.service" ];
+          unitConfig = {
+            DefaultDependencies = false;
+          };
+          serviceConfig = {
+            Type = "oneshot";
+            RemainAfterExit = true;
+            ExecStart = generateDiskKey;
+          };
+        };
       };
     };
   };
