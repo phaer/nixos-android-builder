347855432073bcee7ff993da31d0097b66b0c760
phaer <hello@phaer.org>
2025-08-27 21:26:17 +0200

    android-build-env: disable ldso.nix module...
    and mock its option to avoid conflicts. ldso.nix can't be
    fully disabled upstream currently. It still would add a tmpfiles
    rule regarding /lib/ld-linux.so.2 which we don't want

--- android-build-env.nix
+++ android-build-env.nix
@@ -109,35 +109,58 @@ let
     m
   '';
 in
-lib.mkMerge [
-  {
-    system.build.fhsEnv = fhsEnv;
-    fileSystems."/bin" = {
-      device = "${fhsEnv}/bin";
-      options = [ "bind" ];
-      fsType = "none";
+{
+  disabledModules = [
+    # Allows symlinking /lib/ld-linux.so.2 & friends, but we need a real binary
+    # - no link - there. If its options are set to `null`, upstreams `ldso`
+    # module will add a tmpfiles rule to remove anything at /lib/ld-linux.so.2.
+    # Even though this fails as /lib is read-only at run-time, that causes an
+    # error in the system log. So we disable the `ldso` module.
+    "config/ldso.nix"
+  ];
+  options =
+    let
+      # Mock ldso.nix options, so that we don't have to disable a bunch of modules
+      # which are "disabled" but still evaluated by default (nix-ld, ld-stub, etc)
+      mock = lib.mkOption {
+        type = lib.types.nullOr lib.types.path;
+        default = null;
+      };
+    in
+    {
+      environment.ldso = mock;
+      environment.ldso32 = mock;
     };
+  config = lib.mkMerge [
+    {
+      system.build.fhsEnv = fhsEnv;
+      fileSystems."/bin" = {
+        device = "${fhsEnv}/bin";
+        options = [ "bind" ];
+        fsType = "none";
+      };
 
-    fileSystems."/lib" = {
-      device = "${fhsEnv}/lib";
-      options = [ "bind" ];
-      fsType = "none";
-    };
+      fileSystems."/lib" = {
+        device = "${fhsEnv}/lib";
+        options = [ "bind" ];
+        fsType = "none";
+      };
 
-    fileSystems."/lib64" = {
-      device = "${fhsEnv}/lib";
-      options = [ "bind" ];
-      fsType = "none";
-    };
-  }
-  {
-    environment.variables = {
-      "PATH" = "$PATH:/bin";
-      "SOURCE_DIR" = "$HOME/source";
-    };
-    environment.systemPackages = [
-      fetchAndroid
-      buildAndroid
-    ];
-  }
-]
+      fileSystems."/lib64" = {
+        device = "${fhsEnv}/lib";
+        options = [ "bind" ];
+        fsType = "none";
+      };
+    }
+    {
+      environment.variables = {
+        "PATH" = "$PATH:/bin";
+        "SOURCE_DIR" = "$HOME/source";
+      };
+      environment.systemPackages = [
+        fetchAndroid
+        buildAndroid
+      ];
+    }
+  ];
+}
