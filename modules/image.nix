253647bd4c19d622a2527f9b7fdbc1fadc8fd45c
phaer <hello@phaer.org>
2025-09-01 14:17:28 +0200

    merge resize-var-lib into image.nix

--- modules/image.nix
+++ modules/image.nix
@@ -31,7 +31,18 @@
     # writable /usr/ file system. That's not the case for us, so we disable
     # it and add a bind mount from /usr/bin to /bin while building the image
     # below.
-    system.activationScripts.usrbinenv = lib.mkForce "";
+    system = {
+      image = {
+        id = config.system.name;
+        version = config.system.nixos.version;
+      };
+      # Disable activation script that tries to create /usr/bin/env at runtime,
+      # as that will fail with a verity-backed, read-only /usr
+      activationScripts.usrbinenv = lib.mkForce "";
+    };
+
+    # Updating the random seed on /boot can not work with a read-only /boot.
+    systemd.services.systemd-boot-random-seed.enable = lib.mkForce false;
 
     fileSystems =
       let
@@ -89,25 +100,7 @@
         };
       };
 
-    system.image = {
-      id = config.system.name;
-      version = config.system.nixos.version;
-    };
-
-    # Updating the random seed on /boot can not work with a read-only /boot.
-    systemd.services.systemd-boot-random-seed.enable = lib.mkForce false;
-
-    # Link /var/run to /run to appease systemd
-    boot.initrd.systemd.tmpfiles.settings = {
-      "1-var-run" = {
-        "/var/run" = {
-          L = {
-            argument = "/run";
-          };
-        };
-      };
-    };
-
+    ## Build-time configuration of systemd-repart during image build
     image = {
       repart = {
         # OVMF does not work with the default repart sector size of 4096
@@ -162,16 +155,74 @@
             UUID = "4d21b016-b534-45c2-a9fb-5c16e091fd2d"; # Well known
             Format = "ext4";
             Label = "var-lib";
+            # We want to start out with a very small partition in the image, and add
+            # the real minimum size to to systemd.repart.partitions below instead,
+            # in order to resize it during boot.
+            SizeMinBytes = "10M";
           };
         };
       };
     };
 
-    boot.initrd.systemd.repart.extraArgs =
-      let
-        initrdCfg = config.boot.initrd.systemd.repart;
-      in
-      (lib.optionals (initrdCfg.keyFile != null) [ "--key-file=${initrdCfg.keyFile}" ])
-      ++ (lib.optionals (initrdCfg.factoryReset) [ "--factory-reset=true" ]);
+    ## Run-time configuration of systemd-repart on first boot.
+    # Reuse settings of the repart-generated image file on first boot
+    systemd.repart.partitions."30-var-lib" =
+      config.image.repart.partitions."30-var-lib".repartConfig
+      // {
+        SizeMinBytes = "250G";
+        # Tell systemd-repart to re-format and re-encrypt this partition on each boot
+        # if run with --factory-reset, which we do by default.
+        FactoryReset = true;
+        # FIXME: hack to avoid formatting for too long on large disks.
+        SizeMaxBytes = "500G";
+      };
+
+    boot.initrd.systemd = {
+      # Add mkfs, fsck, etc for ext4 to initrd
+      initrdBin = [ pkgs.e2fsprogs ];
+
+      # Link /var/run to /run to appease systemd
+      tmpfiles.settings = {
+        "1-var-run" = {
+          "/var/run" = {
+            L = {
+              argument = "/run";
+            };
+          };
+        };
+      };
+
+      # Run systemd-repart in initrd at boot
+      repart = {
+        enable = true;
+        extraArgs =
+          let
+            initrdCfg = config.boot.initrd.systemd.repart;
+          in
+          (lib.optionals (initrdCfg.keyFile != null) [ "--key-file=${initrdCfg.keyFile}" ])
+          ++ (lib.optionals (initrdCfg.factoryReset) [ "--factory-reset=true" ]);
+      };
+
+      # Link the read-only nix store to /run/systemd/volatile-root before
+      # systemd-repart runs. systemd-repart normally looks for the block device
+      # backing "/", or this path. So this enables systemd-repart to find the
+      # right device at boot.
+      services.link-volatile-root = {
+        description = "Create volatile-root to tell systemd-repart which disk to user";
+        wantedBy = [ "initrd.target" ];
+        before = [ "systemd-repart.service" ];
+        requiredBy = [ "systemd-repart.service" ];
+        unitConfig = {
+          DefaultDependencies = false;
+        };
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+          ExecStart = ''/bin/ln -sf /dev/disk/by-partlabel/${
+            config.image.repart.partitions."30-var-lib".repartConfig.Label
+          } /run/systemd/volatile-root'';
+        };
+      };
+    };
   };
 }
