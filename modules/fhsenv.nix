4a406cc8b99f13f99feb5edb02f26b251f3b084c
phaer <hello@phaer.org>
2025-09-03 16:06:26 +0200

    disable-ldso: split module from fhsenv.nix

--- modules/fhsenv.nix
+++ modules/fhsenv.nix
@@ -67,27 +67,6 @@ let
   fhsEnv = (pkgs.callPackage ../packages/fhsenv { }) { inherit pins storePaths; };
 in
 {
-  disabledModules = [
-    # Allows symlinking /lib/ld-linux.so.2 & friends, but we need a real binary
-    # - no link - there. If its options are set to `null`, upstreams `ldso`
-    # module will add a tmpfiles rule to remove anything at /lib/ld-linux.so.2.
-    # Even though this fails as /lib is read-only at run-time, that causes an
-    # error in the system log. So we disable the `ldso` module.
-    "config/ldso.nix"
-  ];
-  options =
-    let
-      # Mock ldso.nix options, so that we don't have to disable a bunch of modules
-      # which are "disabled" but still evaluated by default (nix-ld, ld-stub, etc)
-      mock = lib.mkOption {
-        type = lib.types.nullOr lib.types.path;
-        default = null;
-      };
-    in
-    {
-      environment.ldso = mock;
-      environment.ldso32 = mock;
-    };
   config = lib.mkMerge [
     {
       system.build.fhsEnv = fhsEnv;
