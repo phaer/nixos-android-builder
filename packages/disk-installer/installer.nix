3cca5ae0d804687c140a14b0b9111f5d5e724124
phaer <phaer@phaer.org>
2025-11-12 16:22:57 +0100

    installer: fix image boot, add missing kernel modules

--- packages/disk-installer/installer.nix
+++ packages/disk-installer/installer.nix
@@ -9,29 +9,41 @@ let
   disk-installer = pkgs.callPackage ./. { };
   cfg = config.diskInstaller;
 in
-  {
-    
+{
+  
 
-  imports = [
-    "${modulesPath}/image/repart.nix"
-  ];
+imports = [
+  "${modulesPath}/image/repart.nix"
+  "${modulesPath}/profiles/qemu-guest.nix"
+];
 
-  options.diskInstaller = {
-      debug = (lib.mkEnableOption "verbose logging and a debug shell") // { default  = true; };
-    };
+options.diskInstaller = {
+    debug = (lib.mkEnableOption "verbose logging and a debug shell") // { default  = true; };
+  };
 
-    config = {
-      system.stateVersion = "25.11";
-      system.name = "disk-installer";
+  config = {
+    system.stateVersion = "25.11";
+    system.name = "disk-installer";
 
-      # noop settings to appease nixos modules system
-      boot.loader.grub.enable = false;
-      fileSystems."/" = {
+    # noop settings to appease nixos modules system
+    boot.loader.grub.enable = false;
+    fileSystems = {
+      "/" = {
         device = "none";
         fsType = "tmpfs";
       };
+    };
 
-      virtualisation.vmVariant.virtualisation = {
+    boot.initrd.kernelModules = [
+      "virtio_blk"
+      "virtio_pci"
+      "vfat"
+      "nls_cp437"
+      "nls_iso8859-1"
+    ];
+
+    virtualisation.vmVariant = {
+      virtualisation = {
         diskImage = "${config.system.build.image}/${config.image.filePath}";
         cores = 8;
         memorySize = 1024 * 8;
@@ -63,24 +75,28 @@ in
           }
         ];
       };
+    };
 
-      boot.kernelParams = lib.optionals cfg.debug [
-        "rd.systemd.debug_shell=tty1"
+    boot.kernelParams = lib.optionals cfg.debug [
+      "rd.systemd.debug_shell=tty1"
+    ];
+
+    image.repart = {
+
+      mkfsOptions.erofs = [
+        "-zlz4"
+        "-Efragments,ztailpacking"
       ];
+      sectorSize = 512;
+      name = config.system.name;
 
-      image.repart = {
-        mkfsOptions.erofs = [
-          "-zlz4"
-          "-Efragments,ztailpacking"
-        ];
-        sectorSize = 512;
-        name = config.system.name;
-
-        partitions = {
-          "00-esp" = let
+      partitions = {
+        "00-esp" =
+          let
             efiArch = config.nixpkgs.hostPlatform.efiArch;
             efiUki = "/EFI/BOOT/BOOT${lib.toUpper efiArch}.EFI";
-          in {
+          in
+          {
             contents = {
               "${efiUki}".source = "${config.system.build.uki}/nixos.efi";
             };
@@ -91,72 +107,88 @@ in
               SizeMinBytes = "128M";
             };
           };
-        };
+      };
+    };
+
+    boot.initrd.systemd = {
+      emergencyAccess = cfg.debug;
+
+      enable = true;
+      contents."/etc/terminfo".source = "${pkgs.ncurses}/share/terminfo";
+
+      initrdBin = [
+        pkgs.parted
+        pkgs.dosfstools
+        disk-installer.run
+      ];
+      extraBin = {
+        lsblk = "${pkgs.util-linux}/bin/lsblk";
+        tee = "${pkgs.coreutils}/bin/tee";
+        jq = "${pkgs.jq}/bin/jq";
+        ddrescue = "${pkgs.ddrescue}/bin/ddrescue";
+        dialog = "${pkgs.dialog}/bin/dialog";
+        systemd-cat = "${pkgs.systemdMinimal}/bin/systemd-cat";
+        chvt = "${pkgs.kbd}/bin/chvt";
       };
 
-
-      boot.initrd.systemd = {
-        emergencyAccess = cfg.debug;
-
-        enable = true;
-        contents."/etc/terminfo".source = "${pkgs.ncurses}/share/terminfo";
-
-        initrdBin = [
-          pkgs.parted
-          disk-installer.run
-        ];
-        extraBin = {
-          lsblk = "${pkgs.util-linux}/bin/lsblk";
-          tee = "${pkgs.coreutils}/bin/tee";
-          jq = "${pkgs.jq}/bin/jq";
-          ddrescue = "${pkgs.ddrescue}/bin/ddrescue";
-          dialog = "${pkgs.dialog}/bin/dialog";
-          systemd-cat = "${pkgs.systemdMinimal}/bin/systemd-cat";
-          chvt = "${pkgs.kbd}/bin/chvt";
-        };
-
-
-        targets.initrd-switch-root.enable = true;
-        services = {
-          initrd-switch-root.enable = true;
-          initrd-cleanup.enable = true;
-          initrd-parse-etc.enable = false;
-          initrd-nixos-activation.enable = false;
-          initrd-find-nixos-closure.enable = false;
-
-          disk-installer = {
-            description = "Early user prompt during initrd";
-
-            after = [
-              "initrd-root-device.target"
-              "boot.mount"
-            ];
-            wantedBy = [ "initrd.target" ];
-
+      mounts =
+        let
+          esp = config.image.repart.partitions."00-esp".repartConfig;
+        in
+        [
+          {
+            where = "/boot";
+            what = "/dev/disk/by-partlabel/${esp.Label}";
+            type = esp.Format;
             unitConfig = {
               DefaultDependencies = false;
-              ConditionPathExists = "/boot/install_target";
             };
+            requiredBy = [ "initrd-fs.target" ];
+            before = [ "initrd-fs.target" ];
+          }
+        ];
 
-            serviceConfig = {
-              Type = "oneshot";
-              RemainAfterExit = true;
-              StandardInput = "tty-force";
-              StandardOutput = "tty";
-              StandardError = "tty";
-              TTYPath = "/dev/tty2";
-              TTYReset = true;
-              Restart = "no";
-            };
+      targets.initrd-switch-root.enable = true;
+      services = {
+        initrd-switch-root.enable = true;
+        initrd-cleanup.enable = true;
+        initrd-parse-etc.enable = false;
+        initrd-nixos-activation.enable = false;
+        initrd-find-nixos-closure.enable = false;
 
-            onFailure = [ "emergency.target" ];
+        disk-installer = {
+          description = "Early user prompt during initrd";
 
-            environment = {
-              install_source = "/dev/disk/by-partlabel/payload";
-            };
-            script = lib.getExe disk-installer.run;
+          after = [
+            "initrd-root-device.target"
+            "boot.mount"
+          ];
+          wantedBy = [ "initrd.target" ];
+
+          unitConfig = {
+            DefaultDependencies = false;
+            ConditionPathExists = "/boot/install_target";
           };
+
+          serviceConfig = {
+            Type = "oneshot";
+            RemainAfterExit = true;
+            StandardInput = "tty-force";
+            StandardOutput = "tty";
+            StandardError = "tty";
+            TTYPath = "/dev/tty2";
+            TTYReset = true;
+            Restart = "no";
+          };
+
+          onFailure = [ "emergency.target" ];
+
+          environment = {
+            install_source = "/dev/disk/by-partlabel/payload";
+          };
+          script = lib.getExe disk-installer.run;
         };
       };
     };
-  }
+  };
+}
