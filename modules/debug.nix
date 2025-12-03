76344d7a6ce543203ef4b5aeb4ab9011397662e0
phaer <phaer@phaer.org>
2025-12-03 13:26:13 +0100

    debug: disable by default

--- modules/debug.nix
+++ modules/debug.nix
@@ -1,46 +1,52 @@
 # Support debug builds with interactive login & extra software.
-{ pkgs, lib, ... }:
 {
-  # Add extra software from nixpkgs for convinience.
-  environment.systemPackages = with pkgs; [
-    vim
-    htop
-    tmux
-    gitMinimal
-  ];
-
-  # Configure nix with flake support, but no channels.
-  nix = {
-    enable = lib.mkForce true;
-    channel.enable = false;
-    settings.experimental-features = [
-      "nix-command"
-      "flakes"
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  config = lib.mkIf (config.nixosAndroidBuilder.debug) {
+    # Add extra software from nixpkgs for convinience.
+    environment.systemPackages = with pkgs; [
+      vim
+      htop
+      tmux
+      gitMinimal
     ];
+
+    # Configure nix with flake support, but no channels.
+    nix = {
+      enable = lib.mkForce true;
+      channel.enable = false;
+      settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+
+    # Set an empty password for "user"
+    users.users."user" = {
+      initialHashedPassword = "";
+    };
+
+    # Allow password-less sudo for wheel users
+    security.sudo.wheelNeedsPassword = false;
+
+    # Auto-login user
+    services.getty.autologinUser = "user";
+
+    # Enable unauthenticated shell if early boot fails
+    boot.initrd.systemd.emergencyAccess = true;
+
+    # Add verbose log output, to aid debugging boot issues. log_level=debug is available as well.
+    boot.kernelParams = [
+      "systemd.show_status=true"
+      "systemd.log_level=info"
+    ];
+
+    # Add grep to the initrd. Feel free to remove, this just makes
+    # inspection and debugging in an emergency shell much more convinient.
+    boot.initrd.systemd.initrdBin = [ pkgs.gnugrep ];
   };
-
-  # Set an empty password for "user"
-  users.users."user" = {
-    initialHashedPassword = "";
-  };
-
-  # Allow password-less sudo for wheel users
-  security.sudo.wheelNeedsPassword = false;
-
-  # Auto-login user
-  services.getty.autologinUser = "user";
-
-  # Enable unauthenticated shell if early boot fails
-  boot.initrd.systemd.emergencyAccess = true;
-
-  # Add verbose log output, to aid debugging boot issues. log_level=debug is available as well.
-  boot.kernelParams = [
-    "systemd.show_status=true"
-    "systemd.log_level=info"
-    "systemd.log_target=console"
-  ];
-
-  # Add grep to the initrd. Feel free to remove, this just makes
-  # inspection and debugging in an emergency shell much more convinient.
-  boot.initrd.systemd.initrdBin = [ pkgs.gnugrep ];
 }
